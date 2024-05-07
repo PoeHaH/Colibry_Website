9fecba53195bd29860f39d7eea33e6af7768e703
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2024-05-07 11:04:41 +0200

    Employee-card

--- page-about.php
+++ page-about.php
@@ -1,7 +1,170 @@
 <?php get_header(); ?>
 
 <p>
-    Pagina content hier
+    
+<section id="mission" class="mt-8">
+    <div class="container">
+        <h2 class="text-center">
+            <span>Onze</span>
+            <span class="blue-text">missie</span><span class="yellow-text">.</span>
+        </h2>
+        <div class="sub-text">
+            Jouw werkprocessen versterken en de efficiëntie van jouw vastgoedonderneming vergroten
+        </div>
+    </div>
+</section>
+
+<section id="about" class="mt-8">
+    <div class="container">
+        <h2 class="text-center">
+            <span>Over</span>
+            <span class="blue-text">ons</span><span class="yellow-text">.</span>
+        </h2>
+        <p>
+        Colibry is ontstaan uit de ambitie om makelaarskantoren te transformeren tot futureproof spelers in de vastgoedmarkt. Met een sterke focus op datagedreven automatisatie en personalisatie streven we ernaar om de bedrijfsvoering van onze klanten te optimaliseren en een ongeëvenaarde gebruikerservaring te bieden. Bij Colibry geloven we in de kracht van data en AI om groei te realiseren en klantrelaties te versterken. Ons toegewijde team staat klaar om jouw makelaarskantoor futureproof te maken. Ontdek hoe we samen kunnen bouwen aan jouw succes."
+        </p>
+    </div>
+</section>
+
+
+<section id="team" class="mt-8">
+    <div class="container">
+        <div class="box blue-bg no-bottom">
+
+            <h2 class="text-center">
+                <span>Ons</span>
+                <span class="blue-text">team</span><span class="yellow-text">.</span>
+            </h2>
+
+            <div class="row">
+                <div class="col-12 col-lg-3">
+                    <div class="employee-card mt-2">
+                        <div class="employee">
+                            <div>
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/employees/woman.jpg" alt="" loading="lazy" />
+                            </div>
+                            <div class="employee-info">
+                                <span class="employee-name">LALA</span>
+                                <span class="employee-role blue-text">LELE</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-lg-3">
+                    <div class="employee-card mt-2">
+                        <div class="employee">
+                            <div>
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/employees/man.jpg" alt="" loading="lazy" />                           
+                             </div>
+                            <div class="employee-info">
+                                <span class="employee-name">LALA</span>
+                                <span class="employee-role blue-text">LELE</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-lg-3">
+                    <div class="employee-card mt-2">
+                        <div class="employee">
+                            <div>
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/employees/woman.jpg" alt="" loading="lazy" />                           
+                             </div>
+                            <div class="employee-info">
+                                <span class="employee-name">LALA</span>
+                                <span class="employee-role blue-text">LELE</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-lg-3">
+                    <div class="employee-card mt-2">
+                        <div class="employee">
+                            <div>
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/employees/woman.jpg" alt="" loading="lazy" />                           
+                             </div>
+                            <div class="employee-info">
+                                <span class="employee-name">LALA</span>
+                                <span class="employee-role blue-text">LELE</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-lg-3">
+                    <div class="employee-card mt-2">
+                        <div class="employee">
+                            <div>
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/employees/man.jpg" alt="" loading="lazy" />                           
+                             </div>
+                            <div class="employee-info">
+                                <span class="employee-name">LALA</span>
+                                <span class="employee-role blue-text">LELE</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-lg-3">
+                    <div class="employee-card mt-2">
+                        <div class="employee">
+                            <div>
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/employees/woman.jpg" alt="" loading="lazy" />                           
+                             </div>
+                            <div class="employee-info">
+                                <span class="employee-name">LALA</span>
+                                <span class="employee-role blue-text">LELE</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-lg-3">
+                    <div class="employee-card mt-2">
+                        <div class="employee">
+                            <div>
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/employees/woman.jpg" alt="" loading="lazy" />                           
+                             </div>
+                            <div class="employee-info">
+                                <span class="employee-name">LALA</span>
+                                <span class="employee-role blue-text">LELE</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-lg-3">
+                    <div class="employee-card mt-2">
+                        <div class="employee">
+                            <div>
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/employees/man.jpg" alt="" loading="lazy" />                           
+                             </div>
+                            <div class="employee-info">
+                                <span class="employee-name">LALA</span>
+                                <span class="employee-role blue-text">LELE</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+    </div>
+
+</section>
 </p>
 
 <?php get_footer(); ?>
