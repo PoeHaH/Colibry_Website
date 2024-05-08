e641889c47ef727f2237b7eb7cae78abb4ca3555
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2024-05-08 15:42:11 +0200

    Press Responsive

--- page-press.php
+++ page-press.php
@@ -10,9 +10,21 @@
             <span class="yellow-text">.</span>
         </h2>
 
-        <div class="box blue-bg flex-row">
-            <div class="col-12 col-lg-8">
+        <div class="box blue-bg press-container">
+            <div class="col-lg-8 col-12">
                 <h3 class="press-title">Nieuws van ons team</h3>
+
+                <div class="press-card mt-4">
+                    <span class="date">
+                        December 05, 2023
+                    </span>
+                    <span class="press-article mt-1">
+                        Data: de stille kracht van de beste vastgoedkantoren
+                    </span>
+                    <span class="article-link mt-1">
+                        <a href="https://www.immoversity.be/vormingen/data-de-stille-kracht-van-de-beste-vastgoedkantoren">Lees meer</a>
+                    </span>
+                </div>
                 
                 <div class="press-card mt-3">
                     <span class="date">
@@ -22,79 +34,7 @@
                         The Industry's Next Big Thing: Property Portal Personalization
                     </span>
                     <span class="article-link mt-1">
-                        <a href="https://www.fokus-online.be/smart-partner/vind-het-huis-dat-aan-jouw-verwachtingen-voldoet/">Lees meer</a>
-                    </span>
-                </div>
-            
-                <div class="press-card mt-4">
-                    <span class="date">
-                        Februari 15, 2017
-                    </span>
-                    <span class="press-article mt-1">
-                        Deze Vlaamse start-up helpt je in een mum van tijd aan de perfecte woning
-                    </span>
-                    <span class="article-link mt-1">
-                        <a href="https://www.bloovi.be/artikels/stories/2017/deze-vlaamse-start-up-helpt-je-in-een-mum-van-tijd-aan-de-perfecte-woning">Lees meer</a>
-                    </span>
-                </div>
-
-                <div class="press-card mt-4">
-                    <span class="date">
-                        Oktober 18, 2017
-                    </span>
-                    <span class="press-article mt-1">
-                        Wanneer kies je als start-up voor subsidies? Enkele tips en tricks:
-                    </span>
-                    <span class="article-link mt-1">
-                        <a href="https://www.bloovi.be/artikels/stories/2017/wanneer-kies-je-als-start-up-voor-subsidies-enkele-tips-en-tricks">Lees meer</a>
-                    </span>
-                </div>
-
-                <div class="press-card mt-4">
-                    <span class="date">
-                        December 9, 2018
-                    </span>
-                    <span class="press-article mt-1">
-                    Wendy Geeraert: “Als gigantische perfectioniste heb ik moeten leren loslaten en op een team vertrouwen”
-                    </span>
-                    <span class="article-link mt-1">
-                        <a href="https://www.bloovi.be/podcast/wendy-geeraert-als-gigantische-perfectioniste-heb-ik-moeten-leren-loslaten-en-op-een-team-vertrouwen">Lees meer</a>
-                    </span>
-                </div>
-
-                <div class="press-card mt-4">
-                    <span class="date">
-                        Februari 15, 2017
-                    </span>
-                    <span class="press-article mt-1">
-                        Innovatief vastgoedplatform Colibry helpt je in mum van tijd aan perfecte woning
-                    </span>
-                    <span class="article-link mt-1">
-                        <a href="https://start-it-x.prezly.com/innovatief-vastgoedplatform-co-libry-helpt-je-in-mum-van-tijd-aan-perfecte-woning">Lees meer</a>
-                    </span>
-                </div>
-
-                <div class="press-card mt-4">
-                    <span class="date">
-                        Juni 6, 2018
-                    </span>
-                    <span class="press-article mt-1">
-                        Colibry herdefinieert de huizenmarkt: 'de huizenzoeker moet centraal staan'
-                    </span>
-                    <span class="article-link mt-1">
-                        <a href="https://trends.knack.be/geld/immo/co-libry-herdefinieert-de-huizenmarkt-de-huizenzoeker-moet-centraal-staan/">Lees meer</a>
-                    </span>
-                </div>
-
-                <div class="press-card mt-4">
-                    <span class="date">
-                        Maart 18, 2019
-                    </span>
-                    <span class="press-article mt-1">
-                        Vind het huis dat aan jouw verwachtingen voldoet
-                    </span>
-                    <span class="article-link mt-1">
-                        <a href="https://www.vlaio.be/nl/ondernemersverhalen/vind-het-huis-dat-aan-jouw-verwachtingen-voldoet">Lees meer</a>
+                        <a href="https://www.onlinemarketplaces.com/articles/the-industry-s-next-big-thing-property-portal-personalization/">Lees meer</a>
                     </span>
                 </div>
 
@@ -112,37 +52,49 @@
 
                 <div class="press-card mt-4">
                     <span class="date">
-                        Mei 31, 2017
+                        Maart 18, 2019
                     </span>
                     <span class="press-article mt-1">
-                        KBC linkt zijn aanbod aan Co-libry, het nieuwe platform voor de woningzoeker én alumnus van Start it @kbc
+                        Vind het huis dat aan jouw verwachtingen voldoet
                     </span>
                     <span class="article-link mt-1">
-                        <a href="https://newsroom.kbc.com/kbc-linkt-zijn-aanbod-aan-co-libry-het-nieuwe-platform-voor-de-woningzoeker-en-alumnus-van-start-it-kbc">Lees meer</a>
+                        <a href="https://www.vlaio.be/nl/ondernemersverhalen/vind-het-huis-dat-aan-jouw-verwachtingen-voldoet">Lees meer</a>
                     </span>
                 </div>
 
                 <div class="press-card mt-4">
                     <span class="date">
-                        March 5, 2021
+                        December 9, 2018
                     </span>
                     <span class="press-article mt-1">
-                        The Industry's Next Big Thing: Property Portal Personalization
+                    Wendy Geeraert: “Als gigantische perfectioniste heb ik moeten leren loslaten en op een team vertrouwen”
                     </span>
                     <span class="article-link mt-1">
-                        <a href="https://www.fokus-online.be/smart-partner/vind-het-huis-dat-aan-jouw-verwachtingen-voldoet/">Lees meer</a>
+                        <a href="https://www.bloovi.be/podcast/wendy-geeraert-als-gigantische-perfectioniste-heb-ik-moeten-leren-loslaten-en-op-een-team-vertrouwen">Lees meer</a>
                     </span>
                 </div>
 
                 <div class="press-card mt-4">
                     <span class="date">
-                        December 05, 2023
+                        Juni 6, 2018
                     </span>
                     <span class="press-article mt-1">
-                        Data: de stille kracht van de beste vastgoedkantoren
+                        Colibry herdefinieert de huizenmarkt: 'de huizenzoeker moet centraal staan'
                     </span>
                     <span class="article-link mt-1">
-                        <a href="https://www.immoversity.be/vormingen/data-de-stille-kracht-van-de-beste-vastgoedkantoren">Lees meer</a>
+                        <a href="https://trends.knack.be/geld/immo/co-libry-herdefinieert-de-huizenmarkt-de-huizenzoeker-moet-centraal-staan/">Lees meer</a>
+                    </span>
+                </div>
+
+                <div class="press-card mt-4">
+                    <span class="date">
+                        Oktober 18, 2017
+                    </span>
+                    <span class="press-article mt-1">
+                        Wanneer kies je als start-up voor subsidies? Enkele tips en tricks:
+                    </span>
+                    <span class="article-link mt-1">
+                        <a href="https://www.bloovi.be/artikels/stories/2017/wanneer-kies-je-als-start-up-voor-subsidies-enkele-tips-en-tricks">Lees meer</a>
                     </span>
                 </div>
 
@@ -160,6 +112,18 @@
 
                 <div class="press-card mt-4">
                     <span class="date">
+                        Mei 31, 2017
+                    </span>
+                    <span class="press-article mt-1">
+                        KBC linkt zijn aanbod aan Colibry, het nieuwe platform voor de woningzoeker én alumnus van Start it @kbc
+                    </span>
+                    <span class="article-link mt-1">
+                        <a href="https://newsroom.kbc.com/kbc-linkt-zijn-aanbod-aan-co-libry-het-nieuwe-platform-voor-de-woningzoeker-en-alumnus-van-start-it-kbc">Lees meer</a>
+                    </span>
+                </div>
+
+                <div class="press-card mt-4">
+                    <span class="date">
                         Februari 28, 2017
                     </span>
                     <span class="press-article mt-1">
@@ -169,12 +133,33 @@
                         <a href="https://www.dvo.be/artikel/55940-start-up-co-libry-lanceert-digitaal-vastgoedplatform">Lees meer</a>
                     </span>
                 </div>
+            
+                <div class="press-card mt-4">
+                    <span class="date">
+                        Februari 15, 2017
+                    </span>
+                    <span class="press-article mt-1">
+                        Deze Vlaamse start-up helpt je in een mum van tijd aan de perfecte woning
+                    </span>
+                    <span class="article-link mt-1">
+                        <a href="https://www.bloovi.be/artikels/stories/2017/deze-vlaamse-start-up-helpt-je-in-een-mum-van-tijd-aan-de-perfecte-woning">Lees meer</a>
+                    </span>
+                </div>
 
+                <div class="press-card mt-4">
+                    <span class="date">
+                        Februari 15, 2017
+                    </span>
+                    <span class="press-article mt-1">
+                        Innovatief vastgoedplatform Colibry helpt je in mum van tijd aan perfecte woning
+                    </span>
+                    <span class="article-link mt-1">
+                        <a href="https://start-it-x.prezly.com/innovatief-vastgoedplatform-co-libry-helpt-je-in-mum-van-tijd-aan-perfecte-woning">Lees meer</a>
+                    </span>
+                </div>
             </div>
-            <div class="col-lg-1">
 
-            </div>
-            <div class="col-12 col-lg-3">
+            <div class="col-lg-4 col-12">
                 <div class="media-card">
                     <img alt="VASTGOEDNEXT" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/press/VASTGOEDNEXT.png" loading="lazy" />
                     <P>AI REVOLUTIE IN DE VASTGOED SECTOR</P>
